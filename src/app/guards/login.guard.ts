2464b7c28d1bb25152b28191e45a31307d061896
CaiiqueBN <caiquebn.32@gmail.com>
2026-06-29 19:44:14 -0300

    Adiciona "Menu Hamburger"

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -2,16 +2,18 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const loginGuard: CanActivateFn = (route, state) => {
-
-
   const router = inject(Router);
-  const email = sessionStorage.getItem("email");
 
-  if (!email) {
-    alert("Você precisa estar logado para acessar essa página!");
-    router.navigate([""]);
-    return false;
+  if (typeof window !== 'undefined') {
+
+    const email = sessionStorage.getItem("email");
+
+    if (!email) {
+      alert("Você precisa estar logado para acessar essa página!");
+      router.navigate([""]);
+      return false;
+    }
+    return true;
   }
-
-  return true;
+  return false;
 };
